a86ec813c3f9872d3860117068b90dea9b238cf2
jcheyns <33093674+jcheyns@users.noreply.github.com>
2019-05-08 12:19:42 -0500

    back to get

--- src/MDP_HSM.jl
+++ src/MDP_HSM.jl
@@ -129,7 +129,7 @@ nOrders=size(aMDPModel.dfOrders,1)
 @variable(m,FlowExcess[f in aMDPModel.dfFlows[:FlowName]]>=0)
 
 minOcc=@from rds in aMDPModel.dfRounds begin
-    @select rds.RoundName=> (isnull(rds.MinOccurence) ? 0 : rds.MinOccurence) 
+    @select rds.RoundName=>  get( rds.MinOccurence , 0)
     @collect Dict
 end
 
